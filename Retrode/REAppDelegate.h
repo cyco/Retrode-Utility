5a81e8a2a5b4c6a66db44970c44470441c3dcef1
Christoph Leimbrock <christoph.leimbrock@gmx.de>
2012-09-28 15:19:06 +0200

    Initial Commit

---
+++ Retrode/REAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  REAppDelegate.h
+//  Retrode
+//
+//  Created by Christoph Leimbrock on 28.09.12.
+//  Copyright (c) 2012 Christoph Leimbrock. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface REAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ Retrode/REAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  REAppDelegate.m
+//  Retrode
+//
+//  Created by Christoph Leimbrock on 28.09.12.
+//  Copyright (c) 2012 Christoph Leimbrock. All rights reserved.
+//
+
+#import "REAppDelegate.h"
+
+@implementation REAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
